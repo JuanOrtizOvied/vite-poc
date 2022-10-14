f4ed0a3fe231fc7486de2d51adef32403fed9922
Juan Ortiz <juan.ortiz.oviedo@gmail.com>
2022-10-13 22:13:03 -0500

    feat: add png image

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
+import avatarImg from './assets/avatar-example.png'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -26,6 +27,7 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
+      <img src={avatarImg} alt="" />
     </div>
   )
 }
