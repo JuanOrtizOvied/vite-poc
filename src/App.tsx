287ac5a3c4d14fa50f088c6c5814c43ed9fee08a
Juan Ortiz <juan.ortiz.oviedo@gmail.com>
2022-10-13 22:32:00 -0500

    chore: add alias

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import avatarImg from './assets/avatar-example.png'
+import reactLogo from '@assets/react.svg'
+import avatarImg from '@assets/avatar-example.png'
 
 function App() {
   const [count, setCount] = useState(0)
